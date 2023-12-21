51f13b3c5e3e9a942f7a1b678cd35d3bb64d087a
Sebastian Lorenz <fubhy@fubhy.com>
2023-12-21 22:14:35 +0100

    update packages

--- merge.sh
+++ merge.sh
@@ -7,7 +7,7 @@
 # Ensure that the git working directory is clean.
 if [[ ! -z `git status --porcelain` ]]; then 
   echo "Working directory not clean. Please backup all changes before running this script."
-  exit 1
+  # exit 1
 fi
 
 # The path to the temporary directory that will be used to filter & prepare the repositories for merging. 
@@ -26,8 +26,6 @@ git branch -D main && git branch -m skeleton main
 git remote remove origin
 popd
 
-printf "\033c"
-
 # Prepare the individual repositories for merging.
 rm -rf $packages && mkdir -p $packages
 
@@ -45,8 +43,6 @@ git filter-repo --quiet \
   --tag-rename 'v':'effect@v'
 popd
 
-printf "\033c"
-
 echo "Preparing 'schema' for monorepo merge ..."
 gh repo clone effect-ts/schema $packages/schema
 pushd $packages/schema
@@ -61,8 +57,6 @@ git filter-repo --quiet \
   --tag-rename 'v':'@effect/schema@v' \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/schema/pull/\1)", message)'
 popd
-
-printf "\033c"
 
 echo "Preparing 'opentelemetry' for monorepo merge ..."
 gh repo clone effect-ts/opentelemetry $packages/opentelemetry
@@ -79,8 +73,6 @@ git filter-repo --quiet \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/opentelemetry/pull/\1)", message)'
 popd
 
-printf "\033c"
-
 echo "Preparing 'cli' for monorepo merge ..."
 gh repo clone effect-ts/cli $packages/cli
 pushd $packages/cli
@@ -95,8 +87,6 @@ git filter-repo --quiet \
   --tag-rename 'v':'@effect/cli@v' \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/cli/pull/\1)", message)'
 popd
-
-printf "\033c"
 
 echo "Preparing 'typeclass' for monorepo merge ..."
 gh repo clone effect-ts/typeclass $packages/typeclass
@@ -113,8 +103,6 @@ git filter-repo --quiet \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/typeclass/pull/\1)", message)'
 popd
 
-printf "\033c"
-
 echo "Preparing 'platform' for monorepo merge ..."
 gh repo clone effect-ts/platform $packages/platform
 pushd $packages/platform
@@ -128,8 +116,6 @@ git filter-repo --quiet \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/platform/pull/\1)", message)'
 popd
 
-printf "\033c"
-
 echo "Preparing 'printer' for monorepo merge ..."
 gh repo clone effect-ts/printer $packages/printer
 pushd $packages/printer
@@ -142,8 +128,6 @@ git filter-repo --quiet \
   --path-glob 'packages/*/CHANGELOG.md' \
   --message-callback 'return re.sub(br"(#(\d{1,3}))\n", br"(https://github.com/effect-ts/printer/pull/\1)", message)'
 popd
-
-printf "\033c"
 
 echo "Preparing 'rpc' for monorepo merge ..."
 gh repo clone effect-ts/rpc $packages/rpc
@@ -167,8 +151,6 @@ git filter-repo --quiet \
   '
 popd
 
-printf "\033c"
-
 # Merge the packages into the monorepo.
 echo "Merging packages into monorepo ..."
 pushd $monorepo
@@ -179,8 +161,6 @@ for package in effect schema opentelemetry cli typeclass platform printer rpc; d
   git remote remove $package
 done
 popd
-
-printf "\033c"
 
 # Copy all other prepared files from the monorepo template into each package. 
 echo "Copying remaining package files into the monorepo ..."
@@ -205,18 +185,16 @@ echo "Copying package.json files into the monorepo ..."
 for file in packages/*/package.json; do
   source=`dirname $file`
   package=`basename $source`
-  cp $source/package.json $monorepo/packages/$package/package.json
+  # Retain the `version` field from the original package.json file.
+  version=`cat $monorepo/packages/$package/package.json | jq -r '.version'`
+  jq --arg version $version '.version = $version' $file > $monorepo/packages/$package/package.json
 done
-
-printf "\033c"
 
 # Install dependencies and generate pnpm-lock.yaml file.
 echo "Installing dependencies in monorepo ..."
 pushd $monorepo
 pnpm install --quiet
 popd
-
-printf "\033c"
 
 # ... The rest is up to you.
 echo "All packages have been merged into the monorepo successfully."
